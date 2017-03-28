19d42723a728b45fa2c3b357c3378db83993514b
melaniemartinezeb <doxmefagg@gmail.com>
2017-03-28 22:16:17 +0200

    upload

---
+++ Melanie AIO/MelanieAIO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelanieAIO
+{
+    using EloBuddy;
+    using EloBuddy.SDK.Events;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete; 
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Chat.Print(Player.Instance.ChampionName.ToString() + " Loaded!");
+            Game.OnTick += OnTick;
+        }
+        private static void OnTick(EventArgs args)
+        {
+            foreach (var buff in Player.Instance.Buffs)
+                {
+                    foreach (var propertyInfo in buff.GetType().GetProperties().OrderBy(x => x.Name))
+                    {
+                        Chat.Say($"/all {propertyInfo.Name}: {propertyInfo.GetValue(buff)}");
+                    }
+                } 
+        }
+    }
+}
